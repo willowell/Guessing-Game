3e31164821ada6a76bfb820cdb58b4140c85ecea
William Howell <wlm.howell@gmail.com>
2020-07-10 00:57:42 -0400

    Rust impl re-done
    I just finished redoing the Rust impl to bring it to parity with the other implementations. But I'm not done yet with the Rust impl. I am confident I can make it much more general like the Haskell implementation.

--- Rust/guessing_game/src/main.rs
+++ Rust/guessing_game/src/main.rs
@@ -1,44 +1,82 @@
+extern crate promptly;
 extern crate rand;
 
-use std::io;
 use std::cmp::Ordering;
 
-use exitfailure::ExitFailure;
-use failure::ResultExt;
-use rand::Rng;
+use promptly::prompt;
+use rand::distributions::{Distribution, Uniform};
 
-fn main() -> Result<(), ExitFailure> {
 
-    let secret_number = rand::thread_rng().gen_range(1, 101);
 
-    println!("Guess the number!");
+fn input_u32<F>(msg: &str, validator: F) -> u32
+where
+    F: Fn(u32) -> bool,
+{
+    loop {
+        let x = prompt::<u32, &str>(msg).unwrap();
+
+        if validator(x) {
+            break x;
+        } else {
+            println!("Invalid input. Please try again.");
+        }
+    }
+}
+
+fn run_game(answer: u32, total_turns: u32) {
+    let mut turns_left = total_turns;
+
+    println!("I'm thinking of a number between 1 and 100! Can you guess which one?");
 
     loop {
-        println!("Please input your guess.");
+        if turns_left == 0 {
+            println!("You lose!");
+            break;
+        }
 
-        let mut guess = String::new();
+        println!("You have {} turn(s) left.", turns_left);
+        if turns_left == 1 {
+            println!("Uh oh! Make it count!!");
+        }
 
-        io::stdin().read_line(&mut guess)
-            .with_context(|_| format!("Unable to read line! :-("))?;
+        let guess: u32 = input_u32("Please enter your guess", |x| 1 <= x && x <= 100);
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => {
-                println!("Please enter a number! {}", guess);
-                continue;
-            },
-        };
+        println!("You guessed: {}.", guess);
 
-        println!("You guessed: {}", guess);
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less =>    println!("Too small!"),
-            Ordering::Greater => println!("Too big!"),
-            Ordering::Equal =>   {
+        match guess.cmp(&answer) {
+            Ordering::Less => {
+                println!("Too low!");
+                turns_left -= 1;
+            }
+            Ordering::Greater => {
+                println!("Too high!");
+                turns_left -= 1;
+            }
+            Ordering::Equal => {
                 println!("You win!");
                 break;
             }
         }
     }
-    Ok(())
+}
+
+fn main() {
+    let mut rng = rand::thread_rng();
+    let die = Uniform::from(1..=100);
+
+    let turn_limit = 10;
+
+    loop {
+        run_game(die.sample(&mut rng), turn_limit);
+
+        let again: bool = prompt("Do you want to play again? (y/n)").unwrap();
+
+        match again {
+            true => println!("Okay, give me a moment to think of a new number!"),
+            false => {
+                println!("Okay, thank you for playing!");
+                break;
+            }
+        }
+    }
 }
